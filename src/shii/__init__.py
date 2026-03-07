e0a7a3823684860929eb5bab49424d8a1c69109d
Kylen Solvik <kysolvik@gmail.com>
2026-03-06 19:48:58 -0500

    Download various spatial regions as geopandas dataframes

--- src/shii/__init__.py
+++ src/shii/__init__.py
@@ -4,3 +4,4 @@ from ._ems import download_heat_incidents
 from ._311 import download_311_complaints
 from ._weather import download_weather
 from ._misc import download_hvi
+from ._zones import download_zipcodes, download_census_tracts, download_community_districts, download_neighborhoods, download_cd_population
